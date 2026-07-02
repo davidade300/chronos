05c8dbe6cbf69d33ba6ea0eed5277b125fe880db
davidade300 <iaderinho@icloud.comg>
2026-07-02 17:10:35 -0300

    html e css das secoes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import {Heading} from "./components/Heading.tsx";
 
 export function App() {
-    return (
-        <>
-            <Heading attr={123} attr2="String">ola mundo1</Heading>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-                repudiandae, voluptates facere dolorem autem similique tenetur repellat
-                voluptas quaerat nam eaque, dolor et nisi eligendi voluptatem earum
-                delectus sunt sed!
-            </p>
-        </>
-    );
+    return (<>
+        <div className="container">
+            <div className="content">
+                <section>
+                    LOGO
+                </section>
+            </div>
+        </div>
+        <div className="container">
+            <div className="content">
+                <section>
+                    MENU
+                </section>
+            </div>
+        </div>
+        <div className="container">
+            <div className="content">
+                <section>
+                    FORM
+                </section>
+            </div>
+        </div>
+        <div className="container">
+            <div className="content">
+                <section>
+                    FOOTER
+                </section>
+            </div>
+        </div>
+    </>);
 }
 
