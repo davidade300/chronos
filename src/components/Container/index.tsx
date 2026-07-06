ebbcd9f5813345b63fd202d5b996232aa1943a6a
davidade300 <iaderinho@icloud.comg>
2026-07-06 20:26:12 -0300

    criando diretorios para os componentes

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,16 @@
+import type {ReactNode} from "react";
+import styles from './styles.module.css'
+
+type ContainerProps = {
+  children: ReactNode;
+}
+
+export function Container({children}: ContainerProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </div>
+  );
+}
