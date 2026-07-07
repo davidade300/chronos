fb8e56001cf1207c9acebd57f2722624836e6351
davidade300 <iaderinho@icloud.comg>
2026-07-07 14:48:59 -0300

    Componente de logo criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import "./styles/theme.css";
-import "./styles/global.css";
 import {Container} from "./components/Container";
 import {Heading} from "./components/Heading";
+import {Logo} from "./components/Logo";
+
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Heading>LOGO</Heading>
-      </Container>
-      <Container>
-        <Heading>MENU</Heading>
-      </Container>
+  return (<>
+    <Container>
+      <Logo/>
+    </Container>
+    <Container>
+      <Heading>MENU</Heading>
+    </Container>
 
-    </>
-  );
+  </>);
 }
 
