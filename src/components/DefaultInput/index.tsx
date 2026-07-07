bb9b4cffdecb0bb7ab552245d76189bafb779108
davidade300 <iaderinho@icloud.comg>
2026-07-07 20:48:59 -0300

    pequeno macete para ternario que o false nao é necessario.

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,15 +1,16 @@
+import styles from "./styles.module.css";
 import type {ComponentProps} from "react";
 
 type DefaultInputProps = {
   id: string; // forçando o input a ter um id
+  labelText?: string;
 } & ComponentProps<"input"> // o & faz a união de tipos, requer type e o que tem na união.
 // nesse caso, o type e as propriedas da tag input do html
 
-export function DefaultInput({id, type}: DefaultInputProps) {
-  return (
-    <>
-      <label htmlFor={id}>task</label>
-      <input id={id} type={type}/>
-    </>
-  )
+export function DefaultInput({id, type, labelText}: DefaultInputProps) {
+  return (<>
+    {labelText && <label htmlFor={id}>{labelText ? labelText : ""} </label>}
+    {/*se tiver o label text, exibe o label text*/}
+    <input id={id} type={type}/>
+  </>)
 }
