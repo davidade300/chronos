6e938900f6a9ff760a25188eec7b8440f1e83793
davidade300 <iaderinho@icloud.comg>
2026-07-07 15:50:06 -0300

    form inicial.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {Container} from "./components/Container";
-import {Heading} from "./components/Heading";
 import {Logo} from "./components/Logo";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import {Menu} from "./components/Menu";
+import {CountDown} from "./components/CountDown";
 
 export function App() {
   return (<>
@@ -11,7 +12,32 @@ export function App() {
       <Logo/>
     </Container>
     <Container>
-      <Heading>MENU</Heading>
+      <Menu/>
+    </Container>
+
+    <Container>
+      <CountDown/>
+    </Container>
+
+    <Container>
+      <form className="form" action="">
+        <div className="formRow">
+          <label htmlFor="meuInput">task</label>
+          <input id="meuInput" type="text"/>
+        </div>
+        <div className="formRow">
+          <p>lorem ipsum dolor sit amet</p>
+        </div>
+        <div className="formRow">
+          <p>Ciclos</p>
+          <p>0 0 0 0</p>
+        </div>
+        <div className="formRow">
+          <button>
+            Enviar
+          </button>
+        </div>
+      </form>
     </Container>
 
   </>);
