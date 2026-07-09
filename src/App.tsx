9ee845e52c1cb6c012d45351b25c7f075ec2cbde
davidade300 <iaderinho@icloud.comg>
2026-07-09 20:34:45 -0300

    sem lazy initialization

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,16 @@ import {DefaultButton} from "./components/DefaultButton";
 import {PlayCircleIcon} from "lucide-react";
 import {Footer} from "./components/Footer";
 import {Heading} from "./components/Heading";
+import {useState} from "react";
 
 export function App() {
-  let numero = 0;
+  // que todos os componentes que usam a var "numero" saibam da mudança em seu valor
+
+  // Sempre que usar useState, não usar atribuição diretamente
+  const [numero, setNumero] = useState(0);
 
   function handleClick() {
-    const span = document.getElementById("numero");
-    if (!span) return;
-
-    numero += 1;
-    span.innerText = numero.toString();
-    console.log(numero, Date.now());
+    setNumero(prevState => prevState + 1);
   }
 
   return (
