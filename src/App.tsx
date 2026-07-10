c245e14b4d3f96c521ea92a978474d1ba8b58e33
davidade300 <iaderinho@icloud.comg>
2026-07-09 21:38:03 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -10,67 +10,53 @@ import {Cycles} from "./components/Cycles";
 import {DefaultButton} from "./components/DefaultButton";
 import {PlayCircleIcon} from "lucide-react";
 import {Footer} from "./components/Footer";
-import {Heading} from "./components/Heading";
-import {useState} from "react";
 
 export function App() {
-  // que todos os componentes que usam a var "numero" saibam da mudança em seu valor
 
-  // Sempre que usar useState, não usar atribuição diretamente
-  const [numero, setNumero] = useState(() => {
-    return 0;
-  });
+  return (
+    <>
+      <Container>
+        <Logo/>
+      </Container>
+      <Container>
+        <Menu/>
+      </Container>
 
-  function handleClick() {
-    setNumero(prevState => prevState + 1);
-  }
+      <Container>
+        <CountDown/>
+      </Container>
 
-  return (<>
-    <Heading>Número: <span id="numero">{numero}</span></Heading>
-    <button onClick={handleClick}>Aumenta</button>
+      <Container>
+        <form className="form" action="">
 
-    <Container>
-      <Logo/>
-    </Container>
-    <Container>
-      <Menu/>
-    </Container>
+          <div className="formRow">
+            <DefaultInput
+              labelText="task"
+              type="text"
+              id="meuInput"
+              placeholder="Digite algo"
+              defaultValue="Valor preenchido"
+            />
+          </div>
 
-    <Container>
-      <CountDown/>
-    </Container>
+          <div className="formRow">
+            <p>lorem ipsum dolor sit amet</p>
+          </div>
 
-    <Container>
-      <form className="form" action="">
+          <div className="formRow">
+            <Cycles/>
+          </div>
 
-        <div className="formRow">
-          <DefaultInput
-            labelText={numero.toString()}
-            type="text"
-            id="meuInput"
-            placeholder="Digite algo"
-            defaultValue="Valor preenchido"
-          />
-        </div>
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon/>}/>
+          </div>
+        </form>
+      </Container>
 
-        <div className="formRow">
-          <p>lorem ipsum dolor sit amet</p>
-        </div>
+      <Container>
+        <Footer/>
+      </Container>
 
-        <div className="formRow">
-          <Cycles/>
-        </div>
-
-        <div className="formRow">
-          <DefaultButton icon={<PlayCircleIcon/>}/>
-        </div>
-      </form>
-    </Container>
-
-    <Container>
-      <Footer/>
-    </Container>
-
-  </>);
+    </>);
 }
 
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,42 @@
 import styles from "./styles.module.css";
 import {HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from "lucide-react";
+import {useState} from "react";
 
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-  return <nav className={styles.menu}>
-    <a className={styles.menuLink} href="#">
-      <HouseIcon/>
-    </a>
-    <a className={styles.menuLink} href="#">
-      <HistoryIcon/>
-    </a>
-    <a className={styles.menuLink} href="#">
-      <SettingsIcon/>
-    </a>
-    <a className={styles.menuLink} href="#">
-      <SunIcon/>
-    </a>
-  </nav>;
+
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    })
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <h1>{theme}</h1>
+      <a className={styles.menuLink} href="#" aria-label='Ir para a home' title="ir para a home">
+        <HouseIcon/>
+      </a>
+      <a className={styles.menuLink} href="#" aria-label="Ver historico" title="Ver historico">
+        <HistoryIcon/>
+      </a>
+      <a className={styles.menuLink} href="#" aria-label="Configuracoes" title="Configuracoes">
+        <SettingsIcon/>
+      </a>
+      <a className={styles.menuLink}
+         href="#"
+         aria-label="Mudar tema"
+         title="Mudar tema"
+         onClick={handleThemeChange}>
+        <SunIcon/>
+      </a>
+    </nav>
+  );
 }
