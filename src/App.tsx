a48ff48cdb3daca888b41829fe225f02a8472bf2
davidade300 <iaderinho@icloud.comg>
2026-07-13 16:45:05 -0300

    pasta de templates e pages criadas, pages utilizam templates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { Footer } from './components/Footer';
-import { MainForm } from './components/MainForm';
+import { Home } from './pages/Home';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <MainForm />
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
