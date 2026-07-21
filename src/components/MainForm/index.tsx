13d1c0613ba2996f7ab1cd42b41e60fcde0ed224
davidade300 <iaderinho@icloud.com>
2026-07-21 14:55:48 -0300

    remoção de estado local em MainForm e substituição por uso de useRef.useState -> valor do input em tempo real (validacao, etc) | useRef -> outros casos.

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,26 +2,27 @@ import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { DefaultInput } from '../DefaultInput';
-import { type SubmitEvent, useState } from 'react';
+import { type SubmitEvent, useRef } from 'react';
 
 export function MainForm() {
-  const [taskName, setTaskName] = useState('');
+  // const [taskName, setTaskName] = useState('');
+  const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: SubmitEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    console.log('DEU CERTO');
   }
 
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
-      <h1>{taskName}</h1>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
           type='text'
           id='meuInput'
           placeholder='Digite algo'
-          value={taskName}
-          onChange={e => setTaskName(e.target.value)}
+          ref={taskNameInput}
         />
       </div>
 
